55ed848c19cb01e7682bae2f9bb9b1b1da532f6f
panjiazhi <panjiazhi@linbc.com>
2019-03-25 20:23:44 +0800

    11111111111111

--- BaseUi/BaseUi/src/meshui/mesh/InputTextUi.ts
+++ BaseUi/BaseUi/src/meshui/mesh/InputTextUi.ts
@@ -13,11 +13,50 @@
             super(w, h);
         }
         protected initView(): void {
-       
+
+            this.setInputTxtPos()
             this.addEvets()
         }
-   
- 
+        public destory(): void {
+            if (this.chatHtmlInput) {
+                document.body.removeChild(this.chatHtmlInput);
+                this.chatHtmlInput = null;
+            }
+            super.destory()
+
+        }
+        private chatHtmlInput: HTMLElement;
+        private setInputTxtPos(): void {
+            if (!this.chatHtmlInput) {
+                this.chatHtmlInput = <HTMLElement>document.createElement("input");
+                this.chatHtmlInput.style.position = "absolute";
+                this.chatHtmlInput.style["z-index"] = 100
+                this.chatHtmlInput.style.background = "transparent"
+                this.chatHtmlInput.style.color = "#ffffff";
+                document.body.appendChild(this.chatHtmlInput);
+              //  this.chatHtmlInput.addEventListener("change", (cevt: any) => { this.changeFile(cevt) });
+            }
+
+            this.chatHtmlInput.style.left = 0 + "px";
+            this.chatHtmlInput.style.top = 0 + "px";
+            var tw: number = 50
+            var th: number = 20;
+            this.chatHtmlInput.style.fontSize = String(12) + "px";
+            this.chatHtmlInput.style.width = String(tw) + "px";
+            this.chatHtmlInput.style.height = String(th) + "px";
+            this.chatHtmlInput.value = "99.99"
+
+            this.resize()
+        }
+
+        protected resize(): void {
+            super.resize()
+         
+            
+            this.chatHtmlInput.style.left = (this.textureContext.left + this.x) + "px";
+            this.chatHtmlInput.style.top = (this.textureContext.top + this.y) + "px";
+
+        }
         public set text(value: string) {
             LabelTextFont.writeSingleLabel(this.ui.uiRender.uiAtlas, this.ui.skinName, value, 26, TextAlign.LEFT, "#ffffff", "#27262e");
         }
