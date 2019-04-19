8cfe951944ae627196cea5cb94b3e7092b991850
pan3dme <3423526@qq.com>
2019-04-19 23:57:45 +0800

    可以完美选到LAYA图层

--- BaseUi/BaseUi/jsbin/src/laya3d/LayaLaunch.js
+++ BaseUi/BaseUi/jsbin/src/laya3d/LayaLaunch.js
@@ -25,92 +25,22 @@ var WebGLContext = laya.webgl.WebGLContext;
 */
 var Temp3D;
 (function (Temp3D) {
-    var OtherShader = /** @class */ (function (_super) {
-        __extends(OtherShader, _super);
-        function OtherShader() {
-            var _this = this;
-            var vs = "attribute vec2 position;attribute vec2 texcoord;attribute vec4 color;uniform vec2 size;uniform mat4 mmat;varying vec2 v_texcoord;varying vec4 v_color;void main(){vec4 pos =mmat*vec4(position.x,position.y,0,1.0);gl_Position = vec4((pos.x/size.x-0.5)*2.0, (0.5-pos.y/size.y)*2.0, pos.z, 1.0);v_color = color;v_texcoord = texcoord;}";
-            var ps = "precision mediump float;varying vec2 v_texcoord;varying vec4 v_color;uniform sampler2D texture;void main(){vec4 t_color = texture2D(texture, v_texcoord);gl_FragColor = t_color;}";
-            _this = _super.call(this, vs, ps, "myShader") || this;
-            return _this;
+    var FBO = /** @class */ (function () {
+        function FBO(value, w, h) {
+            if (w === void 0) { w = 128; }
+            if (h === void 0) { h = 128; }
+            var gl = Scene_data.context3D.renderContext;
+            this.texture = value;
+            this.frameBuffer = gl.createFramebuffer();
+            this.depthBuffer = gl.createRenderbuffer();
+            gl.bindRenderbuffer(gl.RENDERBUFFER, this.depthBuffer);
+            gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, w, h);
+            this.width = w;
+            this.height = h;
         }
-        OtherShader.shader = new OtherShader();
-        return OtherShader;
-    }(Laya.Shader));
-    Temp3D.OtherShader = OtherShader;
-    var OtherShaderValue = /** @class */ (function (_super) {
-        __extends(OtherShaderValue, _super);
-        function OtherShaderValue() {
-            var _this = _super.call(this, 0, 0) || this;
-            var _vlen = 8 * Laya.CONST3D2D.BYTES_PE;
-            _this.position = [2, Laya.WebGLContext.FLOAT, false, _vlen, 0];
-            _this.texcoord = [2, Laya.WebGLContext.FLOAT, false, _vlen, 2 * Laya.CONST3D2D.BYTES_PE];
-            _this.color = [4, Laya.WebGLContext.FLOAT, false, _vlen, 4 * Laya.CONST3D2D.BYTES_PE];
-            return _this;
-        }
-        return OtherShaderValue;
-    }(Laya.Value2D));
-    Temp3D.OtherShaderValue = OtherShaderValue;
-    var OtherLayaRectSprite = /** @class */ (function (_super) {
-        __extends(OtherLayaRectSprite, _super);
-        function OtherLayaRectSprite() {
-            var _this = _super.call(this) || this;
-            _this.iNum = 0;
-            _this.init(null);
-            return _this;
-        }
-        OtherLayaRectSprite.prototype.init = function (texture, vb, ib) {
-            var _this = this;
-            if (vb === void 0) { vb = null; }
-            if (ib === void 0) { ib = null; }
-            this.vBuffer = Laya.VertexBuffer2D.create();
-            this.iBuffer = Laya.IndexBuffer2D.create();
-            this.ibData = new Uint16Array([]);
-            var vbArray;
-            var ibArray;
-            if (vb) {
-                vbArray = vb;
-            }
-            else {
-                vbArray = [];
-                var texWidth = 100 + random(20);
-                var texHeight = 100 + random(20);
-                var red = 1;
-                var greed = 1;
-                var blue = 1;
-                var alpha = 1;
-                vbArray.push(0, 0, 0, 0, red, greed, blue, alpha);
-                vbArray.push(texWidth, 0, 1, 0, red, greed, blue, alpha);
-                vbArray.push(texWidth, texHeight, 1, 1, red, greed, blue, alpha);
-                vbArray.push(0, texHeight, 0, 1, red, greed, blue, alpha);
-            }
-            if (ib) {
-                ibArray = ib;
-            }
-            else {
-                ibArray = [];
-                ibArray.push(0, 1, 3); //从第一个三角形的顶点索引
-            }
-            this.iNum = ibArray.length;
-            this.vbData = new Float32Array(vbArray);
-            this.ibData = new Uint16Array(ibArray);
-            this.vBuffer.append(this.vbData);
-            this.iBuffer.append(this.ibData);
-            this.shaderValue = new OtherShaderValue();
-            this.shaderValue.textureHost = null;
-            Laya.loader.load("res/ui/icon/lyf_64x.png", Laya.Handler.create(this, function (aa) {
-                _this.texture = aa;
-                _this.shaderValue.textureHost = _this.texture;
-            }));
-            this.scale(2, 2);
-            this._renderType |= Laya.RenderSprite.CUSTOM; //设置当前显示对象的渲染模式为自定义渲染模式
-        };
-        OtherLayaRectSprite.prototype.customRender = function (context, x, y) {
-            context.ctx.setIBVB(x, y, (this.iBuffer), (this.vBuffer), this.iNum, null, OtherShader.shader, this.shaderValue, 0, 0);
-        };
-        return OtherLayaRectSprite;
-    }(Laya.Sprite));
-    Temp3D.OtherLayaRectSprite = OtherLayaRectSprite;
+        return FBO;
+    }());
+    Temp3D.FBO = FBO;
 })(Temp3D || (Temp3D = {}));
 var LayaLaunchTexture = /** @class */ (function (_super) {
     __extends(LayaLaunchTexture, _super);
@@ -143,41 +73,81 @@ var LayaLaunch = /** @class */ (function () {
         configurable: true
     });
     LayaLaunch.prototype.init = function () {
+        var _this = this;
         this._canvas = Laya.init(Browser.clientWidth * Browser.pixelRatio, Browser.clientHeight * Browser.pixelRatio, Laya.WebGL);
         Pan3d.Scene_data.fileRoot = "res/";
         Pan3d.Engine.init(this._canvas);
         var pic = new Laya.Image();
         Laya.stage.addChild(pic);
-        pic.x = 600;
+        pic.x = 300;
         pic.y = 300;
         pic.scale(2, 2);
+        this.outImg = pic;
+        this.makeLayaBaseText();
+        Laya.timer.once(2000, this, function () {
+            console.log(_this.outImg.texture);
+        });
+        this.makeLayaOneText();
+        var picA = new Laya.Image("res/ui/icon/lyf_64x.png");
+        Laya.stage.addChild(picA);
+    };
+    LayaLaunch.prototype.makeLayaOneText = function () {
+        var $ctx = Pan3d.UIManager.getInstance().getContext2D(256, 256, false);
+    };
+    LayaLaunch.prototype.makeLayaBaseText = function () {
+        var _this = this;
         Laya.loader.load("res/ui/icon/objs_64x.png", Laya.Handler.create(this, function (aa) {
-            pic.texture = aa;
-            console.log(aa.uv);
-            LoadManager.getInstance().load(Scene_data.fileRoot + "ui/icon/skill_64x.png", LoadManager.IMG_TYPE, function ($img, $info) {
-                Pan3d.Scene_data.context3D.updateTexture(pic.texture.source, pic.texture.source, pic.texture.source, $img);
-                var tempPanTexture = Pan3d.Scene_data.context3D.getTexture($img);
-                pic.texture.source = tempPanTexture;
-                var a = new LayaLaunchTexture(new laya.resource.Bitmap());
-                //    pic.texture = a
-                for (var keyStr in pic.texture) {
-                    if ("source" == keyStr || "uv" == keyStr || "sourceHeight" == keyStr) {
-                    }
-                    else {
-                        console.log(keyStr, pic.texture[keyStr], a[keyStr]);
-                        a[keyStr] = pic.texture[keyStr];
-                    }
-                }
-                for (var keyStr in a) {
-                    console.log(keyStr, pic.texture[keyStr], a[keyStr]);
-                }
-                a.width = 128;
-                a.height = 128;
-                a.sourceWidth = 128;
-                a.sourceHeight = 128;
-                pic.texture = a;
+            _this.outImg.texture = aa;
+            aa.bitmap.enableMerageInAtlas = false;
+            LoadManager.getInstance().load(Scene_data.fileRoot + "ui/icon/icon_Folder_64x.png", LoadManager.IMG_TYPE, function ($img, $info) {
+                Pan3d.Scene_data.context3D.updateTexture(_this.outImg.texture.source, 0, 0, $img);
+                var knum = $img.width / 128;
+                _this.outImg.texture.uv = [0, 0, knum, 0, knum, knum, 0, knum];
+                _this.fbo = new Temp3D.FBO(_this.outImg.texture.source, 128, 128);
+                _this.outImg.frameLoop(1, _this, _this.upData);
             });
         }));
+    };
+    LayaLaunch.prototype.upData = function () {
+        // console.log(this.outImg)
+        var gl = Scene_data.context3D.renderContext;
+        var rect = new Pan3d.Rectangle(0, 0, gl.canvas.width, gl.canvas.height);
+        this.updateDepthTexture(this.fbo);
+        gl.viewport(0, 0, rect.width, rect.height);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+        gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    };
+    LayaLaunch.prototype.updateDepthTexture = function (fbo) {
+        var gl = Scene_data.context3D.renderContext;
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo.frameBuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fbo.texture, 0);
+        gl.viewport(0, 0, fbo.width, fbo.height);
+        gl.clearColor(Math.random(), 20 / 255, 20 / 255, 1.0);
+        // gl.clearColor(0,0,0,0);
+        gl.clearDepth(1.0);
+        gl.clearStencil(0.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthMask(true);
+        gl.enable(gl.BLEND);
+        gl.frontFace(gl.CW);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
+    };
+    LayaLaunch.prototype.updateDepthTextureCopy = function (fbo) {
+        var gl = Scene_data.context3D.renderContext;
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo.frameBuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fbo.texture, 0);
+        gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, fbo.depthBuffer);
+        gl.viewport(0, 0, fbo.width, fbo.height);
+        gl.clearColor(Math.random(), 20 / 255, 20 / 255, 1.0);
+        // gl.clearColor(0,0,0,0);
+        gl.clearDepth(1.0);
+        gl.clearStencil(0.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthMask(true);
+        gl.enable(gl.BLEND);
+        gl.frontFace(gl.CW);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
     };
     LayaLaunch.initCanvas = function ($caves) {
         var main = new LayaLaunch();
--- BaseUi/BaseUi/src/laya3d/LayaLaunch.ts
+++ BaseUi/BaseUi/src/laya3d/LayaLaunch.ts
@@ -16,87 +16,28 @@ import TextureRes = Pan3d.TextureRes
     自定义着色器
     */
 module Temp3D {
-    export class OtherShader extends Laya.Shader {
-        public static shader: OtherShader = new OtherShader();
-        constructor() {
-            var vs: string = "attribute vec2 position;attribute vec2 texcoord;attribute vec4 color;uniform vec2 size;uniform mat4 mmat;varying vec2 v_texcoord;varying vec4 v_color;void main(){vec4 pos =mmat*vec4(position.x,position.y,0,1.0);gl_Position = vec4((pos.x/size.x-0.5)*2.0, (0.5-pos.y/size.y)*2.0, pos.z, 1.0);v_color = color;v_texcoord = texcoord;}"
-            var ps: string = "precision mediump float;varying vec2 v_texcoord;varying vec4 v_color;uniform sampler2D texture;void main(){vec4 t_color = texture2D(texture, v_texcoord);gl_FragColor = t_color;}";
-            super(vs, ps, "myShader");
-        }
-    }
-    export class OtherShaderValue extends Laya.Value2D {
-        public texcoord: any;
-        constructor() {
-            super(0, 0);
-            var _vlen: number = 8 * Laya.CONST3D2D.BYTES_PE;
-            this.position = [2, Laya.WebGLContext.FLOAT, false, _vlen, 0];
-            this.texcoord = [2, Laya.WebGLContext.FLOAT, false, _vlen, 2 * Laya.CONST3D2D.BYTES_PE];
-            this.color = [4, Laya.WebGLContext.FLOAT, false, _vlen, 4 * Laya.CONST3D2D.BYTES_PE];
-        }
-    }
+    export class FBO {
 
-    export class OtherLayaRectSprite extends Laya.Sprite {
-        protected vBuffer: Laya.VertexBuffer2D;
-        protected iBuffer: Laya.IndexBuffer2D;
-        protected vbData: Float32Array;
-        protected ibData: Uint16Array;
-        protected iNum: number = 0;
-        protected shaderValue: OtherShaderValue;
-        constructor() {
-            super();
-            this.init(null)
-        }
-        public init(texture: Laya.Texture, vb: Array<any> = null, ib: Array<any> = null): void {
-            this.vBuffer = Laya.VertexBuffer2D.create();
-            this.iBuffer = Laya.IndexBuffer2D.create();
-            this.ibData = new Uint16Array([]);
-            var vbArray: Array<any>;
-            var ibArray: Array<any>;
-            if (vb) {
-                vbArray = vb;
-            }
-            else {
-                vbArray = [];
-                var texWidth: number = 100 + random(20)
-                var texHeight: number = 100 + random(20)
-                var red: number = 1;
-                var greed: number = 1;
-                var blue: number = 1;
-                var alpha: number = 1;
-                vbArray.push(0, 0, 0, 0, red, greed, blue, alpha);
-                vbArray.push(texWidth, 0, 1, 0, red, greed, blue, alpha);
-                vbArray.push(texWidth, texHeight, 1, 1, red, greed, blue, alpha);
-                vbArray.push(0, texHeight, 0, 1, red, greed, blue, alpha);
-            }
-            if (ib) {
-                ibArray = ib;
-            }
-            else {
-                ibArray = [];
-                ibArray.push(0, 1, 3);//从第一个三角形的顶点索引
-            }
-            this.iNum = ibArray.length;
-            this.vbData = new Float32Array(vbArray);
-            this.ibData = new Uint16Array(ibArray);
-            this.vBuffer.append(this.vbData);
-            this.iBuffer.append(this.ibData);
-            this.shaderValue = new OtherShaderValue();
 
-            this.shaderValue.textureHost = null;
-            Laya.loader.load("res/ui/icon/lyf_64x.png", Laya.Handler.create(this, (aa: Laya.Texture) => {
+        public width: number
+        public height: number
+        public frameBuffer: WebGLFramebuffer;
+        public depthBuffer: WebGLRenderbuffer;
+        public texture: WebGLRenderbuffer;
+        public constructor(value: WebGLTexture, w: number = 128, h: number = 128) {
+            var gl: WebGLRenderingContext = Scene_data.context3D.renderContext;
+            this.texture = value
 
-                this.texture=aa
-   
-                this.shaderValue.textureHost = this.texture
-            }))
+            this.frameBuffer = gl.createFramebuffer();
+            this.depthBuffer = gl.createRenderbuffer();
 
-            this.scale(2,2)
- 
-            this._renderType |= Laya.RenderSprite.CUSTOM;//设置当前显示对象的渲染模式为自定义渲染模式
+            gl.bindRenderbuffer(gl.RENDERBUFFER, this.depthBuffer);
+            gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, w, h);
+            this.width = w;
+            this.height = h;
         }
-        public customRender(context: Laya.RenderContext, x: number, y: number): void {
-            (context.ctx as Laya.WebGLContext2D).setIBVB(x, y, (this.iBuffer) as Laya.IndexBuffer2D, (this.vBuffer) as Laya.VertexBuffer2D, this.iNum, null, OtherShader.shader, this.shaderValue, 0, 0);
-        }
+
+     
     }
 }
 class LayaLaunchTexture extends Laya.Texture {
@@ -111,7 +52,7 @@ class LayaLaunchTexture extends Laya.Texture {
     public get source(): WebGLTexture {
         return this._baseWebGLTexture
     }
-    private _baseWebGLTexture: WebGLTexture
+    public _baseWebGLTexture: WebGLTexture
 }
 class LayaLaunch {
     private _canvas: HTMLCanvasElement;
@@ -123,75 +64,120 @@ class LayaLaunch {
         this.init()
     }
 
+    private outImg: Laya.Image
     private init(): void {
-
    
         this._canvas = Laya.init(Browser.clientWidth * Browser.pixelRatio, Browser.clientHeight * Browser.pixelRatio, Laya.WebGL);
- 
 
         Pan3d.Scene_data.fileRoot = "res/";
         Pan3d.Engine.init(this._canvas);
  
         var pic: Laya.Image = new Laya.Image();
         Laya.stage.addChild(pic)
-        pic.x = 600;
+        pic.x = 300;
         pic.y = 300;
         pic.scale(2, 2);
 
-   
+        this.outImg = pic;
+ 
+        this.makeLayaBaseText()
+
+
+        Laya.timer.once(2000, this, () => {
+            console.log(this.outImg.texture)
+        })
+
+        this.makeLayaOneText()
+
+
+        var picA: Laya.Image = new Laya.Image("res/ui/icon/lyf_64x.png");
+        Laya.stage.addChild(picA)
+ 
+    }
+    private makeLayaOneText(): void {
+        let $ctx = Pan3d.UIManager.getInstance().getContext2D(256, 256, false);
+ 
+
+       
+    }
+
+    private makeLayaBaseText(): void {
         Laya.loader.load("res/ui/icon/objs_64x.png", Laya.Handler.create(this, (aa: Laya.Texture) => {
-            pic.texture = aa;
-            console.log(aa.uv)
-  
-            LoadManager.getInstance().load(Scene_data.fileRoot + "ui/icon/skill_64x.png", LoadManager.IMG_TYPE, ($img: any, $info: any) => {
+            this.outImg.texture = aa;
+             aa.bitmap.enableMerageInAtlas = false;
+            LoadManager.getInstance().load(Scene_data.fileRoot + "ui/icon/icon_Folder_64x.png", LoadManager.IMG_TYPE, ($img: any, $info: any) => {
+                Pan3d.Scene_data.context3D.updateTexture(this.outImg.texture.source, 0, 0, $img)
 
-               Pan3d.Scene_data.context3D.updateTexture(pic.texture.source, pic.texture.source, pic.texture.source, $img)
+                var knum: number = $img.width / 128;
+                this.outImg.texture.uv = [0, 0, knum, 0, knum, knum, 0, knum]
 
-                var tempPanTexture: WebGLTexture = Pan3d.Scene_data.context3D.getTexture($img)
+                this.fbo = new Temp3D.FBO(this.outImg.texture.source, 128, 128);
 
-                pic.texture.source = tempPanTexture;
-
-
-
- 
-
-              var a: LayaLaunchTexture = new LayaLaunchTexture(new laya.resource.Bitmap())
-
-            //    pic.texture = a
-                for (var keyStr in pic.texture) {
-                    if ("source" == keyStr || "uv" == keyStr || "sourceHeight" == keyStr) {
-                    } else {
-                        console.log(keyStr, pic.texture[keyStr], a[keyStr])
-                        a[keyStr] = pic.texture[keyStr]
-                    }
-                }
-
-                for (var keyStr in a) {
-                    console.log(keyStr, pic.texture[keyStr], a[keyStr])
-
-                }
-                a.width = 128
-                a.height = 128
-                a.sourceWidth = 128
-                a.sourceHeight = 128
-
-                pic.texture = a
-
+                this.outImg.frameLoop(1, this, this.upData)
+          
             })
-
+        }))
  
+    }
+    private fbo: Temp3D.FBO
+    public upData(): void {
+       // console.log(this.outImg)
+        var gl: WebGLRenderingContext = Scene_data.context3D.renderContext
+
+        var rect: Pan3d.Rectangle = new Pan3d.Rectangle(0, 0, gl.canvas.width, gl.canvas.height)
+        this.updateDepthTexture(this.fbo)
+
+
+
+
+        gl.viewport(0, 0, rect.width, rect.height);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+        gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+    }
+
+    private updateDepthTexture(fbo: Temp3D.FBO): void {
+
+        var gl: WebGLRenderingContext = Scene_data.context3D.renderContext
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo.frameBuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fbo.texture, 0);
+
+      
+         gl.viewport(0, 0, fbo.width, fbo.height);
+        gl.clearColor(Math.random(), 20 / 255, 20 / 255, 1.0);
+        // gl.clearColor(0,0,0,0);
+        gl.clearDepth(1.0);
+        gl.clearStencil(0.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthMask(true);
+        gl.enable(gl.BLEND);
+        gl.frontFace(gl.CW);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
+
 
       
 
-        }))
- 
-    
- 
-       
+    }
+
+    private updateDepthTextureCopy(fbo: Temp3D.FBO): void {
+
+        var gl: WebGLRenderingContext = Scene_data.context3D.renderContext
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fbo.frameBuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fbo.texture, 0);
+        gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, fbo.depthBuffer);
+
+        gl.viewport(0, 0, fbo.width, fbo.height);
+        gl.clearColor(Math.random(), 20 / 255, 20 / 255, 1.0);
+        // gl.clearColor(0,0,0,0);
+        gl.clearDepth(1.0);
+        gl.clearStencil(0.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthMask(true);
+        gl.enable(gl.BLEND);
+        gl.frontFace(gl.CW);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
 
 
-  
- 
     }
 
  
