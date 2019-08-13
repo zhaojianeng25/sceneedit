d5597b2517190231771c465e3440440d23461dcd
sceneedit <52847457+zhaojianeng25@users.noreply.github.com>
2019-08-13 20:44:47 +0700

    2

--- mizmain/feijishadow/feijishadow/jsbin/core/TextureUint.js
+++ mizmain/feijishadow/feijishadow/jsbin/core/TextureUint.js
@@ -20,40 +20,107 @@ var core;
     var GlReset = Pan3d.GlReset;
     var ProgrmaManager = Pan3d.ProgrmaManager;
     var TextureManager = Pan3d.TextureManager;
-    var TextureToTextureShader = /** @class */ (function (_super) {
-        __extends(TextureToTextureShader, _super);
-        function TextureToTextureShader() {
+    var Display3D = Pan3d.Display3D;
+    var BaseCavanShader = /** @class */ (function (_super) {
+        __extends(BaseCavanShader, _super);
+        function BaseCavanShader() {
             return _super.call(this) || this;
         }
-        TextureToTextureShader.prototype.binLocation = function ($context) {
-            $context.bindAttribLocation(this.program, 0, "v3Pos");
-            $context.bindAttribLocation(this.program, 1, "v2uv");
+        BaseCavanShader.prototype.binLocation = function ($context) {
+            $context.bindAttribLocation(this.program, 0, "v3Position");
+            $context.bindAttribLocation(this.program, 1, "u2Texture");
         };
-        TextureToTextureShader.prototype.getVertexShaderString = function () {
-            var $str = "attribute vec3 v3Pos;" +
-                "attribute vec3 v2uv;" +
+        BaseCavanShader.prototype.getVertexShaderString = function () {
+            var $str = "attribute vec3 v3Position;" +
+                "attribute vec2 u2Texture;" +
+                "uniform mat4 viewMatrix3D;" +
+                "uniform mat4 camMatrix3D;" +
+                "uniform mat4 posMatrix3D;" +
+                "varying vec2 v_texCoord;" +
                 "void main(void)" +
                 "{" +
-                "   vec4 pos = vec4(v3Pos.xyz,1.0);" +
+                "   v_texCoord = vec2(u2Texture.x, u2Texture.y);" +
+                "   vec4 vt0= vec4(v3Position, 1.0);" +
+                "   vt0 = posMatrix3D * vt0;" +
+                "   vt0 = camMatrix3D * vt0;" +
+                "   vt0 = viewMatrix3D * vt0;" +
                 "   gl_Position = vt0;" +
                 "}";
             return $str;
         };
-        TextureToTextureShader.prototype.getFragmentShaderString = function () {
+        BaseCavanShader.prototype.getFragmentShaderString = function () {
             var $str = "precision mediump float;\n" +
+                "uniform sampler2D s_texture;\n" +
+                "varying vec2 v_texCoord;\n" +
                 "void main(void)\n" +
                 "{\n" +
+                "vec4 infoUv = texture2D(s_texture, v_texCoord.xy);\n" +
                 "gl_FragColor = vec4(1.0,0.0,0.0,1.0); " +
                 "}";
             return $str;
         };
-        TextureToTextureShader.TextureToTextureShader = "TextureToTextureShader";
-        return TextureToTextureShader;
+        BaseCavanShader.BaseCavanShader = "BaseCavanShader";
+        return BaseCavanShader;
     }(Shader3D));
-    core.TextureToTextureShader = TextureToTextureShader;
+    core.BaseCavanShader = BaseCavanShader;
+    var BaseCavanSprite = /** @class */ (function (_super) {
+        __extends(BaseCavanSprite, _super);
+        function BaseCavanSprite() {
+            var _this = _super.call(this) || this;
+            _this.initData();
+            _this.updateMatrix;
+            return _this;
+        }
+        BaseCavanSprite.prototype.initData = function () {
+            ProgrmaManager.getInstance().registe(BaseCavanShader.BaseCavanShader, new BaseCavanShader);
+            this.shader = ProgrmaManager.getInstance().getProgram(BaseCavanShader.BaseCavanShader);
+            this.program = this.shader.program;
+            this.objData = new ObjData;
+            this.objData.vertices = new Array();
+            this.objData.vertices.push(0, 0, 0);
+            this.objData.vertices.push(100, 0, 0);
+            this.objData.vertices.push(100, 0, 100);
+            this.objData.uvs = new Array();
+            this.objData.uvs.push(0, 0);
+            this.objData.uvs.push(1, 0);
+            this.objData.uvs.push(0, 1);
+            this.objData.indexs = new Array();
+            this.objData.indexs.push(0, 1, 2);
+            this.loadTexture();
+            this.upToGpu();
+        };
+        BaseCavanSprite.prototype.loadTexture = function () {
+            var $ctx = UIManager.getInstance().getContext2D(128, 128, false);
+            $ctx.fillStyle = "rgb(255,255,255)";
+            $ctx.fillRect(0, 0, 128, 128);
+            this._uvTextureRes = TextureManager.getInstance().getCanvasTexture($ctx);
+        };
+        BaseCavanSprite.prototype.upToGpu = function () {
+            if (this.objData.indexs.length) {
+                this.objData.treNum = this.objData.indexs.length;
+                this.objData.vertexBuffer = Scene_data.context3D.uploadBuff3D(this.objData.vertices);
+                this.objData.uvBuffer = Scene_data.context3D.uploadBuff3D(this.objData.uvs);
+                this.objData.indexBuffer = Scene_data.context3D.uploadIndexBuff3D(this.objData.indexs);
+            }
+        };
+        BaseCavanSprite.prototype.update = function () {
+            if (this.objData && this.objData.indexBuffer && this._uvTextureRes) {
+                Scene_data.context3D.setProgram(this.program);
+                Scene_data.context3D.setVcMatrix4fv(this.shader, "viewMatrix3D", Scene_data.viewMatrx3D.m);
+                Scene_data.context3D.setVcMatrix4fv(this.shader, "camMatrix3D", Scene_data.cam3D.cameraMatrix.m);
+                Scene_data.context3D.setVcMatrix4fv(this.shader, "posMatrix3D", this.posMatrix.m);
+                Scene_data.context3D.setVa(0, 3, this.objData.vertexBuffer);
+                Scene_data.context3D.setVa(1, 2, this.objData.uvBuffer);
+                Scene_data.context3D.setRenderTexture(this.shader, "s_texture", this._uvTextureRes.texture, 0);
+                Scene_data.context3D.drawCall(this.objData.indexBuffer, this.objData.treNum);
+            }
+        };
+        return BaseCavanSprite;
+    }(Display3D));
+    core.BaseCavanSprite = BaseCavanSprite;
     var TextureUint = /** @class */ (function () {
         function TextureUint() {
-            this.makeBaseRectObjdata();
+            this.baseDiplay3dSprite = new BaseCavanSprite();
         }
         TextureUint.getInstance = function () {
             if (!this._instance) {
@@ -75,19 +142,6 @@ var core;
             ctx.fillRect(0, 0, tempRect.width, tempRect.height);
             TextureManager.getInstance().updateTexture(tempRect.texture, 0, 0, ctx);
             return tempRect;
-        };
-        TextureUint.prototype.makeBaseRectObjdata = function () {
-            ProgrmaManager.getInstance().registe(TextureToTextureShader.TextureToTextureShader, new TextureToTextureShader);
-            this.shader = ProgrmaManager.getInstance().getProgram(TextureToTextureShader.TextureToTextureShader);
-            this.objData = new ObjData();
-            this.objData.vertices.push(-1, 1, 0, 1, 1, 0, 1, -1, 0, -1, -1, 0);
-            this.objData.uvs.push(0, 0, 1, 0, 1, 1, 0, 1);
-            this.objData.indexs.push(0, 1, 2, 0, 3, 2);
-            this.objData.treNum = 6;
-            this.objData.vertexBuffer = Scene_data.context3D.uploadBuff3D(this.objData.vertices);
-            this.objData.uvBuffer = Scene_data.context3D.uploadBuff3D(this.objData.uvs);
-            this.objData.indexBuffer = Scene_data.context3D.uploadIndexBuff3D(this.objData.indexs);
-            this.baseDiplay3dSprite = new Pan3d.BaseDiplay3dSprite();
         };
         TextureUint.prototype.setFboInfo = function (fbo) {
             var gl = Scene_data.context3D.renderContext;
@@ -121,10 +175,11 @@ var core;
             return this.depthFBO.texture;
         };
         TextureUint.prototype.drawBaseRectCavan = function () {
-            Scene_data.context3D.setProgram(this.shader.program);
-            Scene_data.context3D.setVa(0, 3, this.objData.vertexBuffer);
-            Scene_data.context3D.setVa(1, 2, this.objData.uvBuffer);
-            Scene_data.context3D.drawCall(this.objData.indexBuffer, this.objData.treNum);
+            this.baseDiplay3dSprite.update();
+            //Scene_data.context3D.setProgram(this.shader.program);
+            //Scene_data.context3D.setVa(0, 3, this.objData.vertexBuffer);
+            //Scene_data.context3D.setVa(1, 2, this.objData.uvBuffer);
+            //Scene_data.context3D.drawCall(this.objData.indexBuffer, this.objData.treNum);
         };
         return TextureUint;
     }());
--- mizmain/feijishadow/feijishadow/src/core/TextureUint.ts
+++ mizmain/feijishadow/feijishadow/src/core/TextureUint.ts
@@ -6,33 +6,51 @@
     import GlReset = Pan3d.GlReset
     import ProgrmaManager = Pan3d.ProgrmaManager
     import TextureManager = Pan3d.TextureManager
+    import Display3D = Pan3d.Display3D
 
-
-    export class TextureToTextureShader extends Shader3D {
-        static TextureToTextureShader: string = "TextureToTextureShader";
+    export class BaseCavanShader extends Shader3D {
+        static BaseCavanShader: string = "BaseCavanShader";
         constructor() {
             super();
         }
         binLocation($context: WebGLRenderingContext): void {
-            $context.bindAttribLocation(this.program, 0, "v3Pos");
-            $context.bindAttribLocation(this.program, 1, "v2uv");
+            $context.bindAttribLocation(this.program, 0, "v3Position");
+            $context.bindAttribLocation(this.program, 1, "u2Texture");
         }
         getVertexShaderString(): string {
             var $str: string =
-                "attribute vec3 v3Pos;" +
-                "attribute vec3 v2uv;" +
+                "attribute vec3 v3Position;" +
+                "attribute vec2 u2Texture;" +
+
+                "uniform mat4 viewMatrix3D;" +
+                "uniform mat4 camMatrix3D;" +
+                "uniform mat4 posMatrix3D;" +
+
+                "varying vec2 v_texCoord;" +
+
                 "void main(void)" +
                 "{" +
-                "   vec4 pos = vec4(v3Pos.xyz,1.0);" +
+
+                "   v_texCoord = vec2(u2Texture.x, u2Texture.y);" +
+                "   vec4 vt0= vec4(v3Position, 1.0);" +
+                "   vt0 = posMatrix3D * vt0;" +
+                "   vt0 = camMatrix3D * vt0;" +
+                "   vt0 = viewMatrix3D * vt0;" +
                 "   gl_Position = vt0;" +
                 "}"
             return $str
+
+
         }
         getFragmentShaderString(): string {
             var $str: string =
                 "precision mediump float;\n" +
+                "uniform sampler2D s_texture;\n" +
+                "varying vec2 v_texCoord;\n" +
+
                 "void main(void)\n" +
                 "{\n" +
+                "vec4 infoUv = texture2D(s_texture, v_texCoord.xy);\n" +
                 "gl_FragColor = vec4(1.0,0.0,0.0,1.0); " +
                 "}"
             return $str
@@ -40,6 +58,76 @@
         }
 
     }
+
+    export class BaseCavanSprite extends Display3D {
+
+        constructor() {
+            super();
+            this.initData()
+            this.updateMatrix
+        }
+        protected initData(): void {
+            ProgrmaManager.getInstance().registe(BaseCavanShader.BaseCavanShader, new BaseCavanShader);
+            this.shader = ProgrmaManager.getInstance().getProgram(BaseCavanShader.BaseCavanShader);
+            this.program = this.shader.program;
+
+            this.objData = new ObjData;
+            this.objData.vertices = new Array();
+            this.objData.vertices.push(0, 0, 0);
+            this.objData.vertices.push(100, 0, 0);
+            this.objData.vertices.push(100, 0, 100);
+
+            this.objData.uvs = new Array()
+            this.objData.uvs.push(0, 0);
+            this.objData.uvs.push(1, 0);
+            this.objData.uvs.push(0, 1);
+
+            this.objData.indexs = new Array();
+            this.objData.indexs.push(0, 1, 2);
+
+            this.loadTexture();
+
+
+            this.upToGpu()
+
+
+        }
+        private loadTexture(): void {
+            var $ctx: CanvasRenderingContext2D = UIManager.getInstance().getContext2D(128, 128, false);
+            $ctx.fillStyle = "rgb(255,255,255)";
+            $ctx.fillRect(0, 0, 128, 128);
+            this._uvTextureRes = TextureManager.getInstance().getCanvasTexture($ctx)
+        }
+        public _uvTextureRes: TextureRes
+
+        public upToGpu(): void {
+            if (this.objData.indexs.length) {
+                this.objData.treNum = this.objData.indexs.length
+                this.objData.vertexBuffer = Scene_data.context3D.uploadBuff3D(this.objData.vertices);
+                this.objData.uvBuffer = Scene_data.context3D.uploadBuff3D(this.objData.uvs);
+                this.objData.indexBuffer = Scene_data.context3D.uploadIndexBuff3D(this.objData.indexs);
+            }
+        }
+        public update(): void {
+            if (this.objData && this.objData.indexBuffer && this._uvTextureRes) {
+                Scene_data.context3D.setProgram(this.program);
+                Scene_data.context3D.setVcMatrix4fv(this.shader, "viewMatrix3D", Scene_data.viewMatrx3D.m);
+                Scene_data.context3D.setVcMatrix4fv(this.shader, "camMatrix3D", Scene_data.cam3D.cameraMatrix.m);
+                Scene_data.context3D.setVcMatrix4fv(this.shader, "posMatrix3D", this.posMatrix.m);
+
+                Scene_data.context3D.setVa(0, 3, this.objData.vertexBuffer);
+                Scene_data.context3D.setVa(1, 2, this.objData.uvBuffer);
+
+                Scene_data.context3D.setRenderTexture(this.shader, "s_texture", this._uvTextureRes.texture, 0);
+
+                Scene_data.context3D.drawCall(this.objData.indexBuffer, this.objData.treNum);
+
+            }
+
+        }
+
+    }
+     
     export class TextureUint {
 
 
@@ -51,7 +139,7 @@
             return this._instance;
         }
         public constructor() {
-            this.makeBaseRectObjdata();
+            this.baseDiplay3dSprite = new BaseCavanSprite()
         }
         //创建颜色纹理
         public makeColorTexture(colorStr: string = "#ff0000", w: number = 1024, h: number = 1024): TextureRes {
@@ -66,31 +154,10 @@
 
             return tempRect
         }
-        private objData: ObjData;
-        private shader: Shader3D;
-        private makeBaseRectObjdata(): void {
-            ProgrmaManager.getInstance().registe(TextureToTextureShader.TextureToTextureShader, new TextureToTextureShader)
-            this.shader = ProgrmaManager.getInstance().getProgram(TextureToTextureShader.TextureToTextureShader);
-            this.objData = new ObjData();
-            this.objData.vertices.push(
-                -1, 1, 0,
-                1, 1, 0,
-                1, -1, 0,
-                -1, -1, 0);
-            this.objData.uvs.push(
-                0, 0,
-                1, 0,
-                1, 1,
-                0, 1);
-            this.objData.indexs.push(0, 1, 2, 0, 3, 2);
-            this.objData.treNum = 6;
-            this.objData.vertexBuffer = Scene_data.context3D.uploadBuff3D(this.objData.vertices);
-            this.objData.uvBuffer = Scene_data.context3D.uploadBuff3D(this.objData.uvs);
-            this.objData.indexBuffer = Scene_data.context3D.uploadIndexBuff3D(this.objData.indexs);
-
-            this.baseDiplay3dSprite = new Pan3d.BaseDiplay3dSprite()
-        }
-        private baseDiplay3dSprite: Pan3d.BaseDiplay3dSprite
+ 
+      
+         
+        private baseDiplay3dSprite: BaseCavanSprite
         private setFboInfo(fbo: mars3D.MarFBO): void {
             var gl: WebGLRenderingContext = Scene_data.context3D.renderContext
             gl.bindFramebuffer(gl.FRAMEBUFFER, fbo.frameBuffer);
@@ -130,10 +197,13 @@
             return this.depthFBO.texture
         }
         public drawBaseRectCavan(): void {
-            Scene_data.context3D.setProgram(this.shader.program);
-            Scene_data.context3D.setVa(0, 3, this.objData.vertexBuffer);
-            Scene_data.context3D.setVa(1, 2, this.objData.uvBuffer);
-            Scene_data.context3D.drawCall(this.objData.indexBuffer, this.objData.treNum);
+
+            this.baseDiplay3dSprite.update()
+
+            //Scene_data.context3D.setProgram(this.shader.program);
+            //Scene_data.context3D.setVa(0, 3, this.objData.vertexBuffer);
+            //Scene_data.context3D.setVa(1, 2, this.objData.uvBuffer);
+            //Scene_data.context3D.drawCall(this.objData.indexBuffer, this.objData.treNum);
  
         }
 
